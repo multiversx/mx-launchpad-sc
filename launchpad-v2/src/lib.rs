0bde972786d95297b0fc30aaceaf49f1fcb4075c
dorin.iancu <dorin.iancu@elrond.com>
2022-05-24 16:33:56 +0300

    initial copy

---
+++ launchpad-v2/src/lib.rs
@@ -0,0 +1,53 @@
+#![no_std]
+
+elrond_wasm::imports!();
+elrond_wasm::derive_imports!();
+
+use launchpad_common::{launch_stage::Flags, *};
+
+#[elrond_wasm::contract]
+pub trait Launchpad:
+    launchpad_common::LaunchpadMain
+    + launch_stage::LaunchStageModule
+    + config::ConfigModule
+    + setup::SetupModule
+    + tickets::TicketsModule
+    + winner_selection::WinnerSelectionModule
+    + ongoing_operation::OngoingOperationModule
+    + permissions::PermissionsModule
+    + blacklist::BlacklistModule
+    + token_send::TokenSendModule
+    + user_interactions::UserInteractionsModule
+{
+    #[allow(clippy::too_many_arguments)]
+    #[init]
+    fn init(
+        &self,
+        launchpad_token_id: TokenIdentifier,
+        launchpad_tokens_per_winning_ticket: BigUint,
+        ticket_payment_token: TokenIdentifier,
+        ticket_price: BigUint,
+        nr_winning_tickets: usize,
+        confirmation_period_start_epoch: u64,
+        winner_selection_start_epoch: u64,
+        claim_start_epoch: u64,
+    ) {
+        let flags = Flags {
+            has_winner_selection_process_started: false,
+            were_tickets_filtered: false,
+            were_winners_selected: false,
+            was_additional_step_completed: true, // we have no additional step in basic launchpad
+        };
+        self.init_base(
+            launchpad_token_id,
+            launchpad_tokens_per_winning_ticket,
+            ticket_payment_token,
+            ticket_price,
+            nr_winning_tickets,
+            confirmation_period_start_epoch,
+            winner_selection_start_epoch,
+            claim_start_epoch,
+            flags,
+        );
+    }
+}
