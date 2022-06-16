0e3fcc83ab791ac92834ed4746bd695f8c9ba90a
dorin.iancu <dorin.iancu@elrond.com>
2022-06-16 09:47:42 +0300

    build

--- launchpad-guaranteed-tickets/wasm/src/lib.rs
+++ launchpad-guaranteed-tickets/wasm/src/lib.rs
@@ -5,7 +5,7 @@
 #![no_std]
 
 elrond_wasm_node::wasm_endpoints! {
-    launchpad
+    launchpad_guaranteed_tickets
     (
         addTickets
         addUsersToBlacklist
@@ -13,6 +13,7 @@ elrond_wasm_node::wasm_endpoints! {
         claimTicketPayment
         confirmTickets
         depositLaunchpadTokens
+        distributeGuaranteedTickets
         filterTickets
         getConfiguration
         getLaunchStageFlags
